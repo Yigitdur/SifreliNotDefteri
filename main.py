f93be758ca19fffc6f84e2502bf73f568d9e90c7
Yiğit Dur <ygtdr35@gmail.com>
2023-04-30 01:56:29 +0300

    Update

--- main.py
+++ main.py
@@ -1,6 +1,59 @@
 import tkinter
 from tkinter import *
 from PIL import ImageTk, Image
+from tkinter import messagebox
+import base64
+
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+def text_kaydet():
+    baslik = baslik_entry.get()
+    mesaj = note_text.get("1.0", END)
+    sifre = parola_entry.get()
+
+    if len(baslik) == 0 or len(mesaj) == 0 or len(sifre) == 0:
+        messagebox.showwarning(title="Hata!", message="Lütfen bütün boşlukları doldurun.!")
+    else:
+        #şifreyiaç
+        text_sifrele = encode(sifre,mesaj)
+        try:
+            with open("Şifrelinotlar.txt", "a") as data_file:
+                data_file.write(f"\n{baslik}\n{text_sifrele}")
+        except FileNotFoundError:
+            with open("Şifrelinotlar.txt", "w") as data_file:
+                data_file.write(f"\n{baslik}\n{text_sifrele}")
+        finally:
+            baslik_entry.delete(0, END)
+            note_text.delete("1.0", END)
+            parola_entry.delete(0, END)
+def text_ac():
+    text_sifresiniac = note_text.get("1.0", END)
+    parola_sifresiniac = parola_entry.get()
+
+    if len(text_sifresiniac) == 0 or len(parola_sifresiniac) == 0:
+        messagebox.showwarning(title="Hata!", message="Lütfen bütün boşlukları doldurun.!")
+    else:
+        try:
+            cozulmus_mesaj = decode(parola_sifresiniac, text_sifresiniac)
+            note_text.delete("1.0", END)
+            note_text.insert("1.0", cozulmus_mesaj)
+        except:
+            messagebox.showwarning(title="Hata!", message="Lütfen şifrelenmiş mesajı girin.!")
 
 #Sayfa
 window = Tk()
@@ -8,11 +61,6 @@ window.title("Şifreli Notepad")
 window.minsize(height=700, width=400)
 window.maxsize(height=700, width=400)
 window.config(padx=10, pady=10)
-
-def save_text():
-    filename = "Şifreleri-Notlar.txt"
-    with open(filename, "a") as f:
-        f.write(note_text.get("1.0", END))
 
 #Fotoğraf
 image = Image.open("Cok_gizli.png").resize((100, 100))
@@ -39,17 +87,15 @@ note_text.pack()
 parola_label = tkinter.Label(text="Parolanızı buraya giriniz.", font="Arial")
 parola_label.pack()
 #ParolaEntry
-parola_entry = tkinter.Entry(width=40)
+parola_entry = tkinter.Entry(width=40, show="*")
 parola_entry.pack()
 
 #KaydetVeŞifreleButonu
-save_button = tkinter.Button(text="Kaydet Ve Şifrele", command=save_text)
+save_button = tkinter.Button(text="Kaydet Ve Şifrele", command=text_kaydet)
 save_button.pack()
 
 #ŞifreyiAçButonu
-decrypt_button = tkinter.Button(text="Şifreyi Aç")
+decrypt_button = tkinter.Button(text="Şifreyi Aç", command=text_ac)
 decrypt_button.pack()
-
-
 
 window.mainloop()
